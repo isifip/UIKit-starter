2a08b05759826327ff9306468f6f8ab44fa45ac3
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-27 19:38:26 +0400

    first commit

---
+++ StackViewDemo/StackViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Irakli Sokhaneishvili on 27.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
