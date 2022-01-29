b95cf8c32fd330336983885927e6a75aa6d90282
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-29 17:44:16 +0400

    + Set up 2 customised cells for description and info + connect storyboard and view classes

--- FoodPin/FoodPin/VIew/RestaurantDetailHeaderView.swift
+++ FoodPin/FoodPin/VIew/RestaurantDetailHeaderView.swift
@@ -13,9 +13,18 @@ class RestaurantDetailHeaderView: UIView {
     @IBOutlet var nameLabel: UILabel! {
         didSet {
             nameLabel.numberOfLines = 0
+            if let customFont = UIFont(name: "Nunito-Bold", size: 30.0) {
+                nameLabel.font = UIFontMetrics(forTextStyle: .title1).scaledFont(for: customFont)
+            }
         }
     }
-    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel! {
+        didSet {
+            if let customFont = UIFont(name: "Nunito-Bold", size: 20.0) {
+                typeLabel.font = UIFontMetrics(forTextStyle: .headline).scaledFont(for: customFont)
+            }
+        }
+    }
     @IBOutlet var heartButton: UIButton!
     
 
---
+++ FoodPin/FoodPin/VIew/RestaurantDetailTextCell.swift
@@ -0,0 +1,29 @@
+//
+//  RestaurantDetailTextCell.swift
+//  FoodPin
+//
+//  Created by Irakli Sokhaneishvili on 29.01.22.
+//
+
+import UIKit
+
+class RestaurantDetailTextCell: UITableViewCell {
+
+    @IBOutlet var descriptionLabel: UILabel! {
+        didSet {
+            descriptionLabel.numberOfLines = 0
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
