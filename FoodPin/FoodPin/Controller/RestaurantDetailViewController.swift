2b0f8bfa75c9ac61727fea9c9f53d769ee6a73af
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-29 18:04:23 +0400

    + Update RestaurantDetailViewController and using extension to display data. + Programatically connected tableview to storyboard using delegate method.

--- FoodPin/FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/FoodPin/Controller/RestaurantDetailViewController.swift
@@ -17,6 +17,9 @@ class RestaurantDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         navigationController?.navigationBar.prefersLargeTitles = false
         
         // Configure header View
@@ -32,4 +35,32 @@ class RestaurantDetailViewController: UIViewController {
     }
     
     
+}
+
+extension RestaurantDetailViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: String(describing: RestaurantDetailTextCell.self),
+                for: indexPath) as! RestaurantDetailTextCell
+            cell.descriptionLabel.text = restaurant.description
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: String(describing: RestaurantDetailTwoColumnCell.self),
+                for: indexPath) as! RestaurantDetailTwoColumnCell
+            cell.column1TitleLabel.text = "Address"
+            cell.column1TextLabel.text = restaurant.location
+            cell.column2TitleLabel.text = "Phone"
+            cell.column2TextLabel.text = restaurant.phone
+            return cell
+        default:
+            fatalError("Failed to instantiate the table view cell for detail view controller")
+        }
+    }
 }
