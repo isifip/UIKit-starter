a75efbd2f07249624ffa58c4f81847c116cb532e
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-29 14:30:21 +0400

    + Update data model, added, phone and description

--- FoodPin/FoodPin/Model/Restaurant.swift
+++ FoodPin/FoodPin/Model/Restaurant.swift
@@ -8,21 +8,12 @@
 import Foundation
 
 struct Restaurant: Hashable {
-    var name: String
-    var type: String
-    var location: String
-    var image: String
-    var isFavorite: Bool
+    var name: String = ""
+    var type: String = ""
+    var location: String = ""
+    var phone: String = ""
+    var description: String = ""
+    var image: String = ""
+    var isFavorite: Bool = false
     
-    init(name: String, type: String, location: String, image: String, isFavorite: Bool) {
-        self.name = name
-        self.type = type
-        self.location = location
-        self.image = image
-        self.isFavorite = isFavorite
-    }
-    
-    init() {
-        self.init(name: "", type: "", location: "", image: "", isFavorite: false)
-    }
 }
