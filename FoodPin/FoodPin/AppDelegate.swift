7dee152930bee6663dc9d2f315a609e63e89f1ef
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-01 21:55:56 +0400

    + Change tab bar items and refactored storyboard into different pieces

--- FoodPin/FoodPin/AppDelegate.swift
+++ FoodPin/FoodPin/AppDelegate.swift
@@ -13,6 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        
+        UITabBar.appearance().tintColor = UIColor(named: "NavigationBarTitle")
+        UITabBar.appearance().standardAppearance = tabBarAppearance
         
         return true
     }
