23ce3e8cb7a61cc918ebfafb0db372ce0e4a32b5
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-17 14:33:44 +0400

    + Initial commit, without StoryBoard

---
+++ NetflixClone/NetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by Irakli Sokhaneishvili on 17.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
