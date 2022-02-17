caccb10e863535de8a6ba834f759d3cac680ef1a
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-17 15:44:39 +0400

    + Main image on HomeView

---
+++ NetflixClone/NetflixClone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,36 @@
+//
+//  HeroHeaderUIView.swift
+//  NetflixClone
+//
+//  Created by Irakli Sokhaneishvili on 17.02.22.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
+        
+        return imageView
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+}
