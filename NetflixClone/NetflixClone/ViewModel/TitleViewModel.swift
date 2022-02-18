9ead2fb0b05dcad2884476142bc2656fea9772ab
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-18 20:02:06 +0400

    + TitleViewModel and finished layout for Upcoming tab

---
+++ NetflixClone/NetflixClone/ViewModel/TitleViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitleViewModel.swift
+//  NetflixClone
+//
+//  Created by Irakli Sokhaneishvili on 18.02.22.
+//
+
+import Foundation
+
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
