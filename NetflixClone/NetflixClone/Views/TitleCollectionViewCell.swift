c576617f57a4b40e1333fbd606b53c2168d7907d
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-18 18:21:59 +0400

    + Custom collection view cell

---
+++ NetflixClone/NetflixClone/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  TitleCollectionViewCell.swift
+//  NetflixClone
+//
+//  Created by Irakli Sokhaneishvili on 18.02.22.
+//
+
+import UIKit
+import SDWebImage
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    static let identifier = "TitleCollectionViewCell"
+
+    private let posterImageView: UIImageView = {
+        let imageview = UIImageView()
+        imageview.contentMode = .scaleAspectFill
+        
+        return imageview
+    }()
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(posterImageView)
+    }
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        posterImageView.frame = contentView.bounds
+    }
+    
+    
+    
+    
+    
+    public func configure(with model: String) {
+        guard let url = URL(string: model) else { return }
+        posterImageView.sd_setImage(with: url, completed: nil)
+    }
+}
