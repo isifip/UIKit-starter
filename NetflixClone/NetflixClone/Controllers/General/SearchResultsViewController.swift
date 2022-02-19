061821e543331b07527b2c7ce80d49988c3f78f4
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-19 15:48:15 +0400

    + fetching data by search query

--- NetflixClone/NetflixClone/Controllers/General/SearchResultsViewController.swift
+++ NetflixClone/NetflixClone/Controllers/General/SearchResultsViewController.swift
@@ -9,9 +9,9 @@ import UIKit
 
 class SearchResultsViewController: UIViewController {
     
-    private var titles = [Title]()
+    public var titles = [Title]()
     
-    private let searchResultsCollectionView: UICollectionView = {
+    public let searchResultsCollectionView: UICollectionView = {
         
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width: UIScreen.main.bounds.width / 3 - 10, height: 200)
@@ -42,13 +42,16 @@ class SearchResultsViewController: UIViewController {
 
 extension SearchResultsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return titles.count
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TitleCollectionViewCell.identifier, for: indexPath) as? TitleCollectionViewCell else {
             return UICollectionViewCell()
         }
-        cell.backgroundColor = .blue
+        
+        let title = titles[indexPath.row]
+        
+        cell.configure(with: title.posterPath ?? "")
         
         return cell
     }
