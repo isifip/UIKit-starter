2fec43fa22748737b8098a6bab8eadf4cd63fe9f
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-19 17:35:48 +0400

    + TitlePreviewViewController

---
+++ NetflixClone/NetflixClone/Controllers/General/TitlePreviewViewController.swift
@@ -0,0 +1,53 @@
+//
+//  TitlePreviewViewController.swift
+//  NetflixClone
+//
+//  Created by Irakli Sokhaneishvili on 19.02.22.
+//
+
+import UIKit
+import WebKit
+
+class TitlePreviewViewController: UIViewController {
+    
+    private let webView = WKWebView()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        
+        return label
+    }()
+    
+    private let overviewLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    private let downloadButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .red
+        button.setTitle("Download", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.addSubview(webView)
+        view.addSubview(titleLabel)
+        view.addSubview(overviewLabel)
+        view.addSubview(downloadButton)
+            
+    }
+    
+
+}
