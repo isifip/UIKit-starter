04555b38a1fe3e5f063583150af68f7458b168ba
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-21 16:02:03 +0400

    + Deleted Main.StoryBoard, Initial commit

---
+++ AutoLayoutProgrammatically/AutoLayoutProgrammatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutProgrammatically
+//
+//  Created by Irakli Sokhaneishvili on 21.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
