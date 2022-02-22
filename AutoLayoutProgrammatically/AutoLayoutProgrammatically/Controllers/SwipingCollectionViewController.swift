6d8e4543d9d384f65e316f870807eb20c2a720ac
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-22 18:55:17 +0400

    + Lanscape support

--- AutoLayoutProgrammatically/AutoLayoutProgrammatically/Controllers/SwipingCollectionViewController.swift
+++ AutoLayoutProgrammatically/AutoLayoutProgrammatically/Controllers/SwipingCollectionViewController.swift
@@ -11,6 +11,22 @@ private let reuseIdentifier = "Cell"
 
 class SwipingCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        
+        coordinator.animateAlongsideTransition(in: view , animation: { _ in
+            self.collectionViewLayout.invalidateLayout()
+            
+            if self.pageControl.currentPage == 0 {
+                self.collectionView.contentOffset = .zero
+            } else {
+                let indexPath = IndexPath(item: self.pageControl.currentPage, section: 0)
+                self.collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            }
+            
+        }, completion: nil)
+   
+    }
+    
     let pages = [
         Page(imageName: "Blob",
              headerText: "Joins us todat in our fun and games!",
