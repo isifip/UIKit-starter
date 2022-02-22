033a4c7bdcfab7d3ade4550f25d7937b9c99ac80
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-22 18:11:23 +0400

    + Displaying buttons and PageControl inside SwipingCollectionView

--- AutoLayoutProgrammatically/AutoLayoutProgrammatically/Controllers/SwipingCollectionViewController.swift
+++ AutoLayoutProgrammatically/AutoLayoutProgrammatically/Controllers/SwipingCollectionViewController.swift
@@ -20,8 +20,60 @@ class SwipingCollectionViewController: UICollectionViewController, UICollectionV
              bodyText: "Get notified of the savings immediately when we announce them on our website. Make sure to also give us any feedback you have"),
         Page(imageName: "Image2",
              headerText: "VIP members special services",
-             bodyText: "Something")
+             bodyText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
     ]
+    
+    let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.gray, for: .normal)
+        
+        return button
+    }()
+    
+    let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.mainColor, for: .normal)
+        
+        return button
+    }()
+    
+    private let pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 4
+        pc.currentPageIndicatorTintColor = .mainColor
+        pc.pageIndicatorTintColor = UIColor(red: 213/255, green: 185/255, blue: 237/255, alpha: 1)
+        
+        return pc
+    }()
+    
+    private func setupButtonControls() {
+        //view.addSubview(previousButton)
+        
+        let bottomControlsStackView = UIStackView(arrangedSubviews: [previousButton, pageControl, nextButton])
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomControlsStackView.distribution = .fillEqually
+        
+        view.addSubview(bottomControlsStackView)
+        
+        
+        let previousButtonConstraints = [
+            bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50)
+            
+        ]
+        
+        NSLayoutConstraint.activate(previousButtonConstraints)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +82,8 @@ class SwipingCollectionViewController: UICollectionViewController, UICollectionV
         collectionView.register(PageCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         collectionView.isPagingEnabled = true
 
+        
+        setupButtonControls()
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
