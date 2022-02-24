2092e8cefc526d1e4a35d2e60c3063bfd018cb2f
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-24 16:56:42 +0400

    + TableView for HomeVIew

--- Starbucks/Starbucks/Home/HomeViewController.swift
+++ Starbucks/Starbucks/Home/HomeViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 class HomeViewController: StarBucksViewController {
     
     let headerView = HomeHeaderView()
+    var tableView = UITableView()
+    
+    let cellId = "cellId"
+    let tiles = [
+        "Star balance",
+        "Bonus stars",
+        "Try these",
+        "Welcome back",
+        "Uplifting"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavBar()
-        
         setTabBarItem(imageName: "house.fill", title: "Home")
+        setupTableView()
         
         style()
         layout()
@@ -28,18 +38,59 @@ class HomeViewController: StarBucksViewController {
 
 }
 
+
+//MARK: --> TablView
+extension HomeViewController: UITableViewDataSource {
+    func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        tableView.tableFooterView = UIView()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        cell.textLabel?.text = tiles[indexPath.row]
+        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tiles.count
+    }
+    
+}
+
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300
+    }
+}
+
+
+//MARK: --> Header
 extension HomeViewController {
     func style() {
         headerView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
     }
     
     func layout() {
         view.addSubview(headerView)
+        view.addSubview(tableView)
         
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: headerView.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
 }
